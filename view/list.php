1ed8a5e8f763f7d3c267dfb4f78f910d11de7c71
Juan C. Bertoia <bertoia114@gmail.com>
2022-03-10 16:49:41 -0300

    se empezo a agregar estilos al proyecto

--- view/list.php
+++ view/list.php
@@ -1,11 +1,12 @@
 <?php if (isset($dataToView["data"])) $allExpenses = $dataToView["data"]; ?>
-<h1>list</h1>
+
 <?php
 if (!empty($allExpenses)) {
 ?>
   <table>
+    <caption>Lista de gastos</caption>
     <thead>
-      <caption>Gastos</caption>
+
       <tr>
         <th>Categoria</th>
         <th>Importe</th>
@@ -21,8 +22,10 @@ if (!empty($allExpenses)) {
       ?>
         <tr>
           <td><?php echo $expense->category; ?></td>
-          <td><?php echo $expense->amount; ?></td>
-          <td><?php echo $expense->details; ?></td>
+          <td><?php echo "$" . $expense->amount; ?></td>
+          <td>
+            <p><?php echo $expense->details; ?></p>
+          </td>
           <td><?php echo  $objController->expenses->changeFormatDateForView($expense->expensesDate); ?></td>
           <td><a href="index.php?controller=Expenses&action=modify&id=<?php echo $expense->id; ?>&view=list">Modificar</a></td>
           <td><a href="index.php?controller=Expenses&action=delete&id=<?php echo $expense->id; ?>&view=list">Borrar</a></td>
